f261f34f8164a94c0534427317fb7053a4bc6423
santy <a.santyvil@gmail.com>
2026-01-21 16:41:14 +0100

    nuevas cosas

---
+++ Basic-santy/01-variables.js
@@ -0,0 +1,28 @@
+
+//los nombres de las variables
+//lower camel case: donde la primera letra de la varibale es minusculo y luego la siguinte mayuscula
+//Worl
+
+//var
+//(como se hacia antes pero no es la mejor, es la original)
+var helloWorld = "!Holaa¡"
+console.log(helloWorld)
+helloWorld = "hola de nuevo"
+console.log(helloWorld)
+////////// se desaconseja
+
+//let - cuando varia
+
+var hello2 = "!Holaa¡"
+console.log(hello2)
+
+hello2 = "adios"
+console.log(hello2)
+
+
+//const - cuando no varias
+const hello3 = "!Holaa pero con const¡"
+console.log(hello3)
+// Error
+// hello3 = "adios pero con const" //no funciona pq es const el valor es constante
+// console.log(hello3)
---
+++ Basic-santy/02-datatypes.js
@@ -0,0 +1,50 @@
+//definimos los tipos
+
+//Primitivos 
+// Cadena de texto (string)
+let name = "Santy"
+let alias = 'santuco'
+//console.log(name)
+
+// Números (number) 64 bits
+let age = 22
+let year = 2026 // Entero
+let height = 1.70 // Decimal
+
+// Booleanos (boolean)
+
+let isTeacher = false
+let isStudent = true
+
+// Undefinied
+// La variable la hemos declarado
+//  pero aun no le hemos dado valor asociado
+let UndefiniedValue
+//console.log(UndefiniedValue)
+
+
+// Null
+
+let nullValue = null
+// Representa que intencionadamente hay una ausencia de valor
+// Después ya veremos, con el paso del flujo pasara a tener valor o que sera null
+
+
+// Symbol 
+
+let mySymbol = Symbol("mysymbol") // es único e immutable(no se modifica)
+//usados para identificadores de propiedades
+
+// BigInt - Abarca memoria para trabajar con tanto numero
+
+let myBigInt = BigInt(4923849284948456456464646023840)
+
+let myBigInt2 = 432984923409384932844564n
+
+
+console.log(typeof myBigInt)
+
+
+// No primitivos
+
+
