cb119c853f9d515bbc345addda36aa309d101d7a
santy <a.santyvil@gmail.com>
2026-01-21 16:12:06 +0100

    primeros pasos

--- Basic-santy/00-helloworld.js
+++ Basic-santy/00-helloworld.js
@@ -8,3 +8,13 @@ console.log("¡Hola, Javascript!")
 console.log('¡Hola, Javascript!')
 console.log(`¡Hola, Javascript!`)
 //hola santy
+
+console.log(5)
+console.log(5 + 5)
+console.log(5 - 5)
+console.log(5 / 5)
+console.log(5 * 5)
+console.log(25 % 3)//resto
+console.log(5 ** 2) //exponente
+
+ 
